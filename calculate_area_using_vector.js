a0cee57a65d010b9c25ccdaedef707dfe62a382f
Felipe Martenexen <felipe.martenexen@gmail.com>
2024-03-09 16:22:53 -0300

    Update calculate_area_using_vector.js

--- calculate_area_using_vector.js
+++ calculate_area_using_vector.js
@@ -1,6 +1,6 @@
 
 // Asset mapbiomas
-var asset = "projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1";
+var asset = "projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1";
 
 // Asset of regions for which you want to calculate statistics
 var assetTerritories = "users/joaovsiqueira1/MAPBIOMAS/ti_uc";
@@ -55,7 +55,7 @@ var years = [
     '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000',
     '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
     '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
-    '2017', '2018', '2019', '2020'
+    '2017', '2018', '2019', '2020', '2021', '2022'
 ];
 
 // Define a Google Drive output folder 
