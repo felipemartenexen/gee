1664d3762693fd29d9d056940eb4259e4e5cbf2d
Felipe Martenexen <felipe.martenexen@gmail.com>
2025-05-02 16:34:36 -0300

    Update shareAsset.js

--- shareAsset.js
+++ shareAsset.js
@@ -1,16 +1,25 @@
-var regions = [1,2,3,4,5,6,7,8,9];
+var regions = [1];
 
-regions.forEach(function(e){
-  
-  var asset_id = "projects/earthengine-legacy/assets/users/geomapeamentoipam/COLECAO_FOGO_SENTINEL/CLASSIFICACAO/AMAZONIA/queimada_amazonia_v9_13_region"+ e + "_2025_2"
+var years = [];
+for (var y = 2000; y <= 2023; y++) {
+  years.push(y);
+}
 
-  var acl = {
-      "owners": ["geomapeamento.ipam@gmail.com"],  
-      'all_users_can_read': true
-  }
+years.forEach(function(y){
   
-  ee.data.setAssetAcl(asset_id, acl)
+    regions.forEach(function(e){
+    
+    var asset_id = "projects/ee-felipe-martenexen/assets/fire_col4/fireAgain/image/col4_r" + e +"_v5_" + y;
   
-  print("O asset " + asset_id + "agora é público e pode ser acessado por qualquer usuário.");
+    var acl = {
+        "owners": ["luiz.felipe@ipam.org.br"],  
+        'all_users_can_read': true
+    };
+    
+    ee.data.setAssetAcl(asset_id, acl);
+    
+    print("O asset " + asset_id + "agora é público e pode ser acessado por qualquer usuário.");
+    
+  });
   
-})
+});
